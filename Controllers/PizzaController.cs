1e0580ec6825761f458cec06063648598a8775ea
jacoposergio <djago32@gmail.com>
2022-11-22 16:46:56 +0100

    Aggiungere i file di progetto.

---
+++ Controllers/PizzaController.cs
@@ -0,0 +1,52 @@
+﻿using la_mia_pizzeria_static.Data;
+using la_mia_pizzeria_static.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace la_mia_pizzeria_static.Controllers
+{
+    public class PizzaController : Controller
+    {
+
+        PizzeriaDbContext db;
+        public PizzaController() : base()
+        {
+            db = new PizzeriaDbContext();
+        }
+
+        public IActionResult Index()
+        {
+
+            List<Pizza> listaPizza = db.Pizze.ToList();
+
+            return View(listaPizza);
+        }
+
+        public IActionResult Detail(int id)
+        {
+
+            Pizza pizza = db.Pizze.Where(p => p.Id == id).FirstOrDefault();
+            return View(pizza);
+        }
+
+        public IActionResult Create()
+        {
+          
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Pizza pizza)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+
+            db.Pizze.Add(pizza);
+            db.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+    }
+}
